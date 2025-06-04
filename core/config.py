95992fc86f2370a341c11633bfb91f19aadec765
Luiz Carlos Souza Garcia <lcsouzagarcia@gmail.com>
2025-06-04 00:29:03 -0300

    fix: adapted setting to my pc

--- core/config.py
+++ core/config.py
@@ -3,15 +3,19 @@ from typing import List
 
 @dataclass
 class Config:
+    #Audio configration for downloading and processing
     AUDIO_FORMAT: str ="bestaudio/best"
-    TARGET_DOWNLOAD_PATH: str = f"static/audio.%(ext)s"
-    POST_PROCESSORS: List[dict] = [
-        {
+    AUDIO_DOWNLOAD_PATH: str = f"static/audio.%(ext)s"
+    POST_PROCESSORS = [{
         'key': 'FFmpegExtractAudio',
         'preferredcodec': 'mp3',
         'preferredquality': '192',
-        }
-    ],
-    FFMPEG_PATH: str = "./ffmpeg/bin"
+        }]
+
+    FFMPEG_PATH: str = "/usr/bin/ffmpeg" # Adjust this path to your ffmpeg installation
     CHUNK_LENGHT_MS = 60 * 1000 
-    
+
+    #Whisper model configuration
+    WHISPER_MODEL: str = "base"
+    TRANSCRIPTION_FORMAT: str = "txt"
+    TRANSCRITPION_PATH: str = "static/transcript.txt"
