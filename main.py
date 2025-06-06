1764ddab6edb3992eefe09070ab036b6c1245f27
Luiz Carlos Souza Garcia <lcsouzagarcia@gmail.com>
2025-06-06 15:23:49 -0300

    feat: option to generate a wordcloud

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from core.audio_operations import download_audio, transcribe_audio
+from core.wordcloud import generate_from_file
 
 def print_menu():
     print("Options: ")
     print("1. Download Audio")
     print("2. Transcribe Audio")
+    print("3. Generate Wordcloud")
 
 opt = None
 while opt != "exit":
@@ -15,6 +17,8 @@ while opt != "exit":
         download_audio("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
     elif opt == "2":
         transcribe_audio(audio_path="static/audio.mp3", output_path="static/transcript.txt")
+    elif opt == "3":
+        generate_from_file(path="./static/transcript.txt")
     elif opt == "exit":
         print("Exiting the program")
     else:
