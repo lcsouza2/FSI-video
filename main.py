5cc3ac2708bb4a862e8e7b361aa10746c403ae4b
Luiz Carlos Souza Garcia <lcsouzagarcia@gmail.com>
2025-06-13 00:26:06 -0300

    feat: main.py menu now has option 4 and uses inputs on args

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from core.audio_operations import download_audio, transcribe_audio
-from core.wordcloud import generate_from_file
+from core.wordcloud import generate_from_file, generate_from_str
 
 def print_menu():
     print("Options: ")
     print("1. Download Audio")
     print("2. Transcribe Audio")
-    print("3. Generate Wordcloud")
+    print("3. Generate Wordcloud from file")
+    print("4. Generate Wordcloud from text")
 
 opt = None
 while opt != "exit":
@@ -14,11 +15,13 @@ while opt != "exit":
     opt = input("Enter an option (or 'exit' to quit): ")
     opt = opt.strip().lower()
     if opt == "1":
-        download_audio("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+        download_audio(input("Url do vídeo: "))
     elif opt == "2":
-        transcribe_audio(audio_path="static/audio.mp3", output_path="static/transcript.txt")
+        transcribe_audio(audio_path=input("Arquivo de áudio: "), output_path=input("Arquivo de destino: "))
     elif opt == "3":
-        generate_from_file(path="./static/transcript.txt")
+        generate_from_file(path=input("Arquivo referência: "), output_file=input("Arquivo de saída: "))
+    elif opt == "4":
+        generate_from_str(text=input("Texto referência: "), output_file=input("Arquivo de saída: "))
     elif opt == "exit":
         print("Exiting the program")
     else:
