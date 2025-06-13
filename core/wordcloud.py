22aad6bb0756afea57491736fec5f65fda59a110
Luiz Carlos Souza Garcia <lcsouzagarcia@gmail.com>
2025-06-13 00:27:00 -0300

    feat: wordcloud.py new function generate_from_str

--- core/wordcloud.py
+++ core/wordcloud.py
@@ -1,12 +1,20 @@
 from wordcloud import WordCloud
-import matplotlib.pyplot as plt
 
-def generate_from_file(path: str):
+def generate_from_file(path: str, output_file: str = "wordcloud"):
+
+    output_file = output_file.split(".")[0]
 
     with open(path) as text_file:
         content = text_file.read()
 
         wordcloud = WordCloud(width=800, height=400, background_color="white").generate_from_text(content)
 
-        wordcloud.to_file("./static/wordcloud.png")
+        wordcloud.to_file(f"{output_file}.png")
 
+def generate_from_str(text: str, output_file: str = "wordcloud"):
+
+    output_file = output_file.split(".")[0]
+
+    wordcloud = WordCloud(width=800, height=400, background_color="white").generate_from_text(text)
+
+    wordcloud.to_file(f"{output_file}.png")
