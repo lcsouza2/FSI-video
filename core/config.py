de8bcbc253a3bdf755dad453672b89afa2789b69
Luiz Carlos Souza Garcia <lcsouzagarcia@gmail.com>
2025-06-24 09:18:31 -0300

    feat\!: new vars to config properly AssemblyAI instead of Whisper

--- core/config.py
+++ core/config.py
@@ -13,9 +13,15 @@ class Config:
         }]
 
     FFMPEG_PATH: str = "/usr/bin/ffmpeg" # Adjust this path to your ffmpeg installation
-    CHUNK_LENGHT_MS = 60 * 1000 
+    CHUNK_LENGHT_MS = 60 * 1000
 
-    #Whisper model configuration
-    WHISPER_MODEL: str = "base"
+    #AssemblyAI model configuration
+    BASE_URL = "https://api.assemblyai.com"
+    AUTH_TOKEN = "b1d3b6047bc54d01a16802521c5f5c6c"
+    SPEECH_MODEL = "universal"
+    DEFAULT_HEADERS: dict = {
+        "authorization": AUTH_TOKEN
+    }
+
     TRANSCRIPTION_FORMAT: str = "txt"
     TRANSCRITPION_PATH: str = "static/transcript.txt"
