4c6be8d3a6dd1ac27a5c12b10291c93adce4214b
Luiz Carlos Souza Garcia <lcsouzagarcia@gmail.com>
2025-06-25 23:16:33 -0300

    style: formats the code with ruff check && format

--- core/wordcloud_gen.py
+++ core/wordcloud_gen.py
@@ -3,8 +3,8 @@ from core.config import Config
 from google import genai
 from google.genai import types
 
-def process_text_with_ai(filepath: str | None, plaintext: str | None):
 
+def process_text_with_ai(filepath: str | None, plaintext: str | None):
     genai_client = genai.Client(api_key=Config.GENAI_API_KEY)
 
     if filepath:
@@ -24,26 +24,30 @@ def process_text_with_ai(filepath: str | None, plaintext: str | None):
         contents=f"Vou gerar uma nuvem de palavras com esse texto, pode remover artigos preposições e etc? responda somente com o texto processado: {texto}",
         config=types.GenerateContentConfig(
             thinking_config=types.ThinkingConfig(thinking_budget=0)
-        )
+        ),
     )
 
     return output.text
 
-def generate_from_file(path: str, output_file: str = "wordcloud"):
 
+def generate_from_file(path: str, output_file: str = "wordcloud"):
     output_file = output_file.split(".")[0]
 
     with open(path) as text_file:
         content = text_file.read()
 
-        wordcloud = WordCloud(width=800, height=400, background_color="white").generate_from_text(content)
+        wordcloud = WordCloud(
+            width=800, height=400, background_color="white"
+        ).generate_from_text(content)
 
         wordcloud.to_file(f"{output_file}.png")
 
-def generate_from_str(text: str, output_file: str = "wordcloud"):
 
+def generate_from_str(text: str, output_file: str = "wordcloud"):
     output_file = output_file.split(".")[0]
 
-    wordcloud = WordCloud(width=800, height=400, background_color="white").generate_from_text(text)
+    wordcloud = WordCloud(
+        width=800, height=400, background_color="white"
+    ).generate_from_text(text)
 
     wordcloud.to_file(f"{output_file}.png")
