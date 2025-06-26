2e2ca4c5c5ac9fd153af740a8b9d7561db5f798b
Luiz Carlos Souza Garcia <lcsouzagarcia@gmail.com>
2025-06-25 23:18:16 -0300

    chore: removes unused variables from Config class

--- core/config.py
+++ core/config.py
@@ -1,28 +1,19 @@
 from dataclasses import dataclass
 
+
 @dataclass
 class Config:
-    #Audio configration for downloading and processing
-    AUDIO_FORMAT: str ="bestaudio/best"
-    AUDIO_DOWNLOAD_PATH: str = f"static/audio.%(ext)s"
-    POST_PROCESSORS = [{
-        'key': 'FFmpegExtractAudio',
-        'preferredcodec': 'mp3',
-        'preferredquality': '192',
-        }]
+    # Audio configration for downloading and processing
+    AUDIO_FORMAT: str = "bestaudio/best"
+    VIDEO_FORMAT: str = "bestvideo+bestaudio/best"
+    POST_PROCESSORS = [
+        {
+            "key": "FFmpegExtractAudio",
+            "preferredcodec": "mp3",
+            "preferredquality": "192",
+        }
+    ]
 
-    FFMPEG_PATH: str = "/usr/bin/ffmpeg" # Adjust this path to your ffmpeg installation
-    CHUNK_LENGHT_MS = 60 * 1000
-
-    #AssemblyAI model configuration
-    BASE_URL = "https://api.assemblyai.com"
-    AUTH_TOKEN = "b1d3b6047bc54d01a16802521c5f5c6c"
-    SPEECH_MODEL = "universal"
-    DEFAULT_HEADERS = {
-        "authorization": AUTH_TOKEN
-    }
-
-    TRANSCRIPTION_FORMAT: str = "txt"
-    TRANSCRITPION_PATH: str = "static/transcript.txt"
+    FFMPEG_PATH: str = "/usr/bin/ffmpeg"  # Adjust this path to your ffmpeg installation
 
     GENAI_API_KEY: str = "AIzaSyB0fB_f5ktPvsTe7znzqxrIgQHEkcHtb1o"
