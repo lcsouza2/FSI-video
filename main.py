771f87d79af107018bccc02425dd2dbcd7cace5d
Luiz Carlos Souza Garcia <lcsouzagarcia@gmail.com>
2025-06-25 23:20:00 -0300

    refactor: creates an web app instead of a CLI

--- main.py
+++ main.py
@@ -1,28 +1,51 @@
-from core.audio_operations import download_audio, transcribe_audio
-from core.wordcloud_gen import generate_from_file, generate_from_str
+from core.audio_operations import Video
+from fastapi import FastAPI, Request, Query, HTTPException
+from fastapi.responses import FileResponse
+from fastapi.middleware.cors import CORSMiddleware
+import os
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
-def print_menu():
-    print("Options: ")
-    print("1. Download Audio")
-    print("2. Transcribe Audio")
-    print("3. Generate Wordcloud from file")
-    print("4. Generate Wordcloud from text")
+MAIN_APP = FastAPI()
+MAIN_APP.mount("/statics", StaticFiles(directory="./templates/statics"), "static")
 
-opt = None
-while opt != "exit":
+templates_folder = Jinja2Templates("./templates/")
+MAIN_APP.add_middleware(CORSMiddleware, allow_origins=["*"])
 
-    print_menu()
-    opt = input("Enter an option (or 'exit' to quit): ")
-    opt = opt.strip().lower()
-    if opt == "1":
-        download_audio(input("Url do vídeo (obrigatório!): "))
-    elif opt == "2":
-        transcribe_audio(audio_path=input("Arquivo de áudio: "), output_path=input("Arquivo de destino: "))
-    elif opt == "3":
-        generate_from_file(path=input("Arquivo referência: "), output_file=input("Arquivo de saída: "))
-    elif opt == "4":
-        generate_from_str(text=input("Texto referência: "), output_file=input("Arquivo de saída: "))
-    elif opt == "exit":
-        print("Exiting the program")
-    else:
-        print("Invalid option, please try again")
+
+@MAIN_APP.get("/")
+async def lading_page(request: Request):
+    return templates_folder.TemplateResponse(request=request, name="index.html")
+
+
+@MAIN_APP.post("/download/audio")
+async def return_downloaded_audio(url: str = Query()):
+    """
+    Downloads the audio from the given video URL using yt-dlp.
+
+    Returns:
+        FileResponse: Response with downloaded audio file.
+    """
+
+    file = Video(url).download_audio()
+
+    if not os.path.exists(file):
+        raise HTTPException(500, "Erro buscando arquivo baixado")
+
+    return FileResponse(path=file, filename="Audio_baixado.mp3")
+
+
+@MAIN_APP.post("/download/video")
+async def return_downloaded_video(url: str = Query()):
+    """
+    Downloads the video from the given video URL using yt-dlp.
+
+    Returns:
+        FileResponse: Response with downloaded audio file.
+    """
+    file = Video(url, "video").download_video()
+
+    if not os.path.exists(file):
+        raise HTTPException(500, "Erro buscando arquivo baixado")
+
+    return FileResponse(path=file, filename="Audio_baixado")
